327aee4f4f78825e27899f19ef440bc9d2964306
MedanXploit12 <75316440+MRKALIT09@users.noreply.github.com>
2021-01-25 21:20:47 +0700

    Add files via upload

---
+++ coli.sh
@@ -0,0 +1,131 @@
+#!bin/bash
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+
+echo $me"[$pur•$me]$ku INSTALL BAHAN DLU COEG KALAU MAU COLI"
+xdg-open https://m.apkpure.com/id/azka-anti-block-browser-unblock-without-vpn/com.azka.browser.anti.blokir
+sleep 5
+echo $me"═════════════════════════════════"
+echo $me" [$pur•$me]$cy CREATOR :$ku MR-KALIT09" | lolcat
+echo $me" [$pur•$me]$cy WHATPSAPP ME :$ku 085361524681" | lolcat
+echo $me" [$pur•$me]$cy https://$ku github.com/MRKALIT09" | lolcat
+echo $me"═════════════════════════════════"
+
+echo "             MASUKAN MENU BOKEP NYA" | lolcat
+echo "            ════════════════════════"| lolcat
+echo "            [1] THE BEST ANGEL JAPAN"| lolcat
+echo "            [2] Jordi And KATANA    "| lolcat
+echo "            [3] Malay Arab          "| lolcat
+echo "            [4] Ngentod di Warung   "| lolcat
+echo "            [5] Colmek Toge Besar   "| lolcat
+echo "            [5] BOKEP JAPAN -1      "| lolcat
+echo "            [6] BOKEP JAPAN -2      "| lolcat
+echo "            [7] MIA KHALIFA         "| lolcat
+echo "            [8] BANG BROS -1        "| lolcat
+echo "            [9] BANG BROS -2        "| lolcat
+echo "            [10]Bang Bros -3        "| lolcat
+echo "            [00] EXIT PROGRAM       "| lolcat
+echo "            ═══════════════════════ "| lolcat
+read -p "         PILIH BAHAN COLI : " pill
+if [ $pill = "1" ]
+then
+echo $me "[$ku•$me]$me ANDA AKAN OTOMATIS DI ARAHKAN KE WEB"
+xdg-open https://www.xnxx.com/video-l3wyj98/japanese_angel_best_of_the_best
+sleep 4
+fi
+#batas gan
+if [ $pill = "2" ]
+then
+echo $me "[$ku•$me] $me ANDA AKAN OTOMATIS DI ARAHKAN KE WEB"
+sleep 4
+xdg-open https://www.xnxx.com/video-v5fzde0/katana_-_reality_kings
+fi
+#batas gan
+if [ $pill = "3" ]
+then
+echo $me "[$ku•$me] $me ANDA AKAN OTOMATIS DI ARAHKAN KE WEB"
+sleep 4
+xdg-open https://www.xnxx.com/video-i83i1ba/malay_arab_girl_fucking_hard_with_boyfriend_-_kacylive.com
+fi
+#batas gan
+if [ $pill = "4" ]
+then
+echo $me "[$ku•$me] $me  ANDA AKAN OTOMATIS DI ARAHKAN KE WEB"
+sleep 4
+xdg-open https://www.xnxx.com/video-101nar04/abis_makan_bakso_lanjut_ngewe
+fi
+#batas gan
+if [ $pill = "5" ]
+then
+echo $me "[ $ku•$me] $me  ANDA AKAN OTOMATIS DI ARAHKAN KE WEB"
+sleep 4
+xdg-open https://www.xnxx.com/video-zq055ec/seragam_sma_toge_colmek
+#batas gan
+if [ $pill = "6" ]
+then
+echo $me "[ $ku•$me] $me  ANDA AKAN OTOMATIS DI ARAHKAN KE WEB"
+sleep 3
+xdg-open https://www.xnxx.com/video-wgjm7eb/ga_mau_tapi_ujung_ujung_nya_menikmati_-_bagian_dua_bit.ly_sensasional
+fi
+#batas gan
+if [ $pill = "7" ]
+then
+echo $me "[ $ku•$me] $me  ANDA AKAN OTOMATIS DI ARAHKAN KE WEB"
+sleep 4
+xdg-open https://www.xnxx.com/video-o8hcbb3/lecehin_kakak_bohay_sampe_puas
+fi
+#batas gan
+if [ $pill = "8" ]
+then
+echo $me "[ $ku•$me] $me  ANDA AKAN OTOMATIS DI ARAHKAN KE WEB"
+sleep 4
+xdg-open https://www.xnxx.com/video-y7yax4f/tanpa_sensor_gadis_jepang_umur_22_tahun_f_cup_pertama_kali_main_di_hotel
+fi
+#batas gan
+if [ $pill = "9" ]
+then
+echo $me "[ $ku•$me] $me  ANDA AKAN OTOMATIS DI ARAHKAN KE WEB"
+sleep 4
+xdg-open https://www.xnxx.com/video-xpnzn0d/bang_bros_18_-_kylie_rocket
+fi
+#batas gan
+if [ $pill = "10" ]
+then
+echo $me "[ $ku•$me] $me  ANDA AKAN OTOMATIS DI ARAHKAN KE WEB"
+sleep 4
+https://www.xnxx.com/video-r2qap4d/bangbros_-_tyler_steel_makes_his_step_sister_alina_lopez_feel_better_after_getting_dumped
+fi
+#batas gan
+if [ $pill = "00" ]
+then
+echo " INGAT SCRIP SEWAKTU-WAKTU BISA RUSAK " | lolcat
+sleep 4
+echo" ANDA SECARA OTOMATIS KE WA/FB SAYA JIKA KAMU" | lolcat
+sleep 4
+echo " MENEKAN EXIT ..INGAT" | lolcat
+sleep 4
+echo " KAMU CHAT SAYA HANYA KARNA SCRIP RUSAK " | lolcat
+sleep 4
+echo " JIKA SCRIP TIDAK RUSAK SAYA TIDAK TANGGAPI!!"  | lolcat
+sleep 4
+echo "  KAMU AKAN DI ARAHKAN KE FB SAYA " | lolcat
+xdg-open https://www.facebook.com/profile.php?id=100027700041268
+fi
+sleep 4
+echo " KAMU AKAN DI ARAHKAN KE WA SAYA " | lolcat
+sleep 4
+xdg-open http://wa.me+6285361524681
+fi
+sleep 4
+echo " ANDA SECARA OTOMATIS AKAN LOG OUT DARI PROGRAM " | lolcat
+sleep 2.5
+echo $me"[$pur•$me]$i LOG OUT SUCCES FULL"
+exit
+fi
+
